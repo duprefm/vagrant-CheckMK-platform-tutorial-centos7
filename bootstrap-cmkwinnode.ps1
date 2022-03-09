a4a57975692b05d338b31cc28093f662cf487bcb
Fabrice Dupre <duprefm@gmail.com>
2022-03-09 08:37:50 +0100

    Add a firewall rule to allow ping in Windos bootstarp file

--- bootstrap-cmkwinnode.ps1
+++ bootstrap-cmkwinnode.ps1
@@ -2,3 +2,7 @@
 # Description: Boxstarter bootstrap script for basic Windows Server configuration
 #Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fdenyTSXonnections" -Value 0
 #Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Import-Module NetSecurity
+
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-Out)" -Enabled True
